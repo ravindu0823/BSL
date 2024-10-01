6e38a1f242da50ad57afd597bd1f9be074219a6f
Ravindu Perera <guestpc87@gmail.com>
2024-10-01 21:14:08 +0530

    Add Flask-CORS for enabling Cross-Origin Resource Sharing

--- app-production.py
+++ app-production.py
@@ -5,8 +5,10 @@ import numpy as np
 import pickle
 import base64
 from gevent.pywsgi import WSGIServer
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the pre-trained models
 with open('one_hand_model.pkl', 'rb') as f:
